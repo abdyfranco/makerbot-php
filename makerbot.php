8283ee00c9efc7ea30fb5565ac9c4d3c6f9e9b86
Abdy Franco <yosoy@abdyfran.co>
2021-06-27 18:07:18 -0600

    Update makerbot.php

--- makerbot.php
+++ makerbot.php
@@ -52,8 +52,6 @@ class Replicator
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 
         if ($method == 'POST') {
             curl_setopt($ch, CURLOPT_POST, true);
